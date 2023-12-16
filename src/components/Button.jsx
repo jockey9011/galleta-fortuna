ea7e18b3e22d6a06b439c7ed50ee24073e90f5bd
Jonathan Rodriguez <jsteven.261190@gmail.com>
2023-12-15 22:28:30 -0500

    galletaFortuna-react-project1

---
+++ src/components/Button.jsx
@@ -0,0 +1,19 @@
+import getRandomFromArr from "../utils/getRandomFromArray"
+import phrases from '../utils/phrases.json'
+import arrPhotos from '../utils/photos.json'
+
+
+const Button = ({ setQuoteRandom, setPhotoRandom }) => {
+
+
+const handleChangePhrase = () => {
+  setQuoteRandom(getRandomFromArr(phrases))
+  setPhotoRandom(getRandomFromArr(arrPhotos))
+}
+
+  return (
+    <button className="app__btn" onClick={handleChangePhrase}>Ver otra frase</button>
+  )
+}
+
+export default Button
---
+++ src/components/Phrase.jsx
@@ -0,0 +1,11 @@
+
+
+const Phrase = ({ quoteRandom }) => {
+
+
+  return (
+    <p className="app__phrase">{ quoteRandom.phrase }</p>
+  )
+}
+
+export default Phrase
